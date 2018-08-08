a6be675b66c30cf4e66941bcfa230ecbc4e19cc8
SurlyMae <surlymae@gmail.com>
2018-08-08 09:30:36 -0600

    Demo requiring own files

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-console.log('Starting app')
+console.log('Starting app.js')
 
 const fs = require('fs')
 const os = require('os')
+const notes = require('./notes.js')
 
-var user = os.userInfo()
+var res = notes.addNote()
+console.log(res)
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err) {
-  if (err) {
-    console.log('Unable to write to file')
-  }
-})
+var sum = notes.add(2, 8)
+console.log(sum)
+
+// var user = os.userInfo()
+
+// fs.appendFile(
+//   'greetings.txt',
+//   `Hello ${user.username}! You are ${notes.age}.`,
+//   function(err) {
+//     if (err) {
+//       console.log('Unable to write to file')
+//     }
+//   }
+// )
